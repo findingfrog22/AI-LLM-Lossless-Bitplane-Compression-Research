1d688495c258258afd52a476ab106aec67264544
findingfrog22 <39844704+findingfrog22@users.noreply.github.com>
2026-02-07 18:43:51 -0500

    Alpha - 0.8.0
    Fixes/Changes:
    - Fixed the bitplane formatting issues that led to incorrect RLE compression analysis results (?)
    - Added averages for further analysis of RLE compression results
    - Added debug feature to add more print information for debugging
    
    To Do for A0.9.0:
    - Introduce LZ4 and ZSTD compression analysis (with configurable block sizes, 4KB default) for each bitplane, and also maybe for each vector embedding
    - Add print information under each printed block of results so that the user can interpret the data more easily
    
    During early beta:
    - Calculate shannon entropy
    - density?
    
    During late beta:
    - Quality of life improvements:
    --> Local file scanning
    --> randomness settings (for file selection, and for row selection)
    --> more explanation on how to use code in comments, along with explanations for simulation parameters + settings

--- Simulations/Wikipedia_DPR/WikipediaQuantizerTest.py
+++ Simulations/Wikipedia_DPR/WikipediaQuantizerTest.py
@@ -2,11 +2,19 @@
 WikipediaDPR_path = "C:/Users/findi/OneDrive/Desktop/AI Research Datasets/Wikipedia DPR Mini/" #base directory of Wikipedia DPR mini dataset
 testfile = WikipediaDPR_path + "Multiset/train-00000-of-00157.parquet" #current file selection
 
-NUM_ROWS = 2048 #this is how many rows of vector embeddings will be processed
+NUM_ROWS = 512 #this is how many rows of vector embeddings will be processed
 NUM_DIMS = 768 #this is the number of weights per vector embedding
 
 #some more global settings
 ACCELERATION_DEVICE = "xpu"
+PRINT_DEBUG = False #determines if you want to print information relevant for debugging
+#this includes:
+# - quantized + dequantized + scale data (for both vertical and horizontal)
+# - bit conversions for quantized and scale data
+# - bitplanes for quantized and scale data
+# [False, default] off
+# [True] on
+
 #symmetric scalar quantization settings
 MULTI_SCALAR = True
 #MULTI_SCALAR determines if you want to do symmetric scalar quantization on your columns of vector embeddings
@@ -16,6 +24,7 @@ MULTI_SCALAR = True
 # --> note: this will print after your per-vector symmetric scalar quantization, as this will always be on regardless
 FLOAT_BITS = 32 #number of bits in input float [FP32 - 32 default, FP64 - 64, etc]
 INT_BITS = 8 #number of bits in the output integer [INT8 - 8 default, INT4 - 4, etc]
+
 #lz4 and zstd compression settings
 BLOCK_SIZE = 4096 #block size for compression, [4KB default - 4096, 8KB - 8192, 16KB - 16384]
 
@@ -32,8 +41,11 @@ def Symmetric_Scalar_Quantization_ROW(tens_emb):
     scales = torch.clamp(scales, min=1e-9) #for now, and these are the scales per vector embedding row
     #third, element-wise quantization and rounding (uses XPU backend to minimize memory trips)
     quantized = torch.round(tens_emb / scales).to(torch.int8) #for now, quantized to int8
-    print(quantized)
-    print(scales)
+    if(PRINT_DEBUG == True):
+        print("\nRow-Wise Symmetric Scalar Quantization (Quantized Values): ")
+        print(quantized)
+        print("\nRow-Wise Symmetric Scalar Quantization (Scalar Values): ")
+        print(scales)
     return quantized, scales
 
 def Symmetric_Scalar_Dequantization_ROW(data, scalars):
@@ -43,25 +55,32 @@ def Symmetric_Scalar_Dequantization_ROW(data, scalars):
     fl = data.to(torch.float32)
     #second, do the multiplication across iGPU
     dequantized = fl * scalars
-    print(dequantized)
+    if(PRINT_DEBUG == True):
+        print("\nRow-Wise Symmetric Scalar Dequantization (Dequantized values): ")
+        print(dequantized)
     return dequantized
 
 def Symmetric_Scalar_Error_ROW(orig, dequant):
     import torch
     import torch.nn.functional as f
     import numpy
+    print("\nRow-Wise Symmetric Scalar Dequantization Error (Mean Square Error, MSE) Stats: ")
     #overall loss
     overall_loss = f.mse_loss(orig, dequant)
+    print("\nOverall Loss (MSE): ")
     print(overall_loss)
     #row-wise
     #elementwise
     element_loss = f.mse_loss(orig, dequant, reduction='none')
+    print("\nElement-Wise Loss (MSE): ")
     print(element_loss)
     #row-wise avgs
     vector_loss = element_loss.mean(dim=1)
+    print("\nLoss per Vector (MSE): ")
     print(vector_loss)
     #finding worst vector
     worst_vector = torch.argmax(vector_loss)
+    print("\nLeast accurate vector (MSE): ")
     print(worst_vector)
     return vector_loss
 
@@ -76,8 +95,11 @@ def Symmetric_Scalar_Quantization_COL(tens_emb):
     scales = torch.clamp(scales, min=1e-9) #for now, and these are the scales per vector embedding row
     #third, element-wise quantization and rounding (uses XPU backend to minimize memory trips)
     quantized = torch.round(tens_emb / scales).to(torch.int8) #for now, quantized to int8
-    print(quantized)
-    print(scales)
+    if(PRINT_DEBUG == True):
+        print("\nColumn-Wise Symmetric Scalar Quantization (Quantized Values): ")
+        print(quantized)
+        print("\nColumn-Wise Symmetric Scalar Quantization (Scalar Values): ")
+        print(scales)
     return quantized, scales
 
 def Symmetric_Scalar_Dequantization_COL(data, scalars):
@@ -87,25 +109,32 @@ def Symmetric_Scalar_Dequantization_COL(data, scalars):
     fl = data.to(torch.float32)
     #second, do the multiplication across iGPU
     dequantized = fl * scalars
-    print(dequantized)
+    if(PRINT_DEBUG == True):
+        print("\nColumn-Wise Symmetric Scalar Dequantization (Dequantized values): ")
+        print(dequantized)
     return dequantized
 
 def Symmetric_Scalar_Error_COL(orig, dequant):
     import torch
     import torch.nn.functional as f
     import numpy
+    print("\nColumn-Wise Symmetric Scalar Dequantization Error (Mean Square Error, MSE) Stats: ")
     #overall loss
     overall_loss = f.mse_loss(orig, dequant)
+    print("\nOverall Loss (MSE): ")
     print(overall_loss)
     #row-wise
     #elementwise
     element_loss = f.mse_loss(orig, dequant, reduction='none')
+    print("\nElement-Wise Loss (MSE): ")
     print(element_loss)
     #col-wise avgs
     vector_loss = element_loss.mean(dim=0)
+    print("\nLoss per column (position in embedding vector of 768 dims) (MSE): ")
     print(vector_loss)
     #finding worst vector
     worst_vector = torch.argmax(vector_loss)
+    print("\nLowest accuracy column (of all embedding vectors of 768 dims) (MSE): ")
     print(worst_vector)
     return vector_loss
 
@@ -216,7 +245,9 @@ def initialization():
     #allows to select specific columns to save memory/time
     datafile_subset = pandas.read_parquet(testfile, columns=['embeddings']) #now we have access to raw embeddings
     raw_embeddings = datafile_subset['embeddings'][:NUM_ROWS]
-    print(raw_embeddings)
+    if(PRINT_DEBUG == True):
+        print("\nDEBUG: raw_embeddings: ")
+        print(raw_embeddings)
     
     #1.) Symmetric Scalar quantization: Raw embedding FP32 --> INT8 + scalar (have 2 versions: per vector scale (this is what you should start with, single and multivector), per column of all vectors (multivector only))
     import torch
@@ -264,13 +295,19 @@ def initialization():
         
     #converts it to binary
     binstring_qr = Pool().map(rowbinary_quant, bit_patterns_cpu)
-    print(binstring_qr) #works
+    if(PRINT_DEBUG == True):
+        print("\nDEBUG: binstring_qr: ")
+        print(numpy.array(binstring_qr)) #works, prints whole thing for debugging
+        #print(binstring_qr) #works, prints abbreviated version
     #scalar bitplane
     bpg_sr = scales.view(torch.uint32)
     bpc_sr = bpg_sr.cpu()
     #converts it to binary
     binstring_sr = Pool().map(rowbinary_scale, bpc_sr)
-    print(binstring_sr) #works
+    if(PRINT_DEBUG == True):
+        print("\nDEBUG: binstring_sr: ")
+        print(numpy.array(binstring_sr)) #works, prints whole thing for debugging
+        #print(binstring_sr) #works, prints abbreviated version
     
     #now we need to do this for the vertical symmetric scalar quantization too, (only if applicable)
     if((NUM_ROWS > 1) and (MULTI_SCALAR == True)):
@@ -279,13 +316,19 @@ def initialization():
         bpc_qc = bpg_qc.cpu()
         #convert it to binary
         binstring_qc = Pool().map(rowbinary_quant, bpc_qc)
-        print(binstring_qc) #works
+        if(PRINT_DEBUG == True):
+            print("\nDEBUG: binstring_qc: ")
+            print(numpy.array(binstring_qc)) #works, prints whole thing for debugging
+            #print(binstring_qc) #works, prints abbreviated version
         #then, we want bitplanes of the scalar data
         bpg_sc = scales_C.view(torch.uint32)
         bpc_sc = bpg_sc.cpu()
         #convert it to binary
         binstring_sc = Pool().map(rowbinary_scale, bpc_sc)
-        print(binstring_sc) #basically works (so close, but lsbs are a little off), probably a rounding thing
+        if(PRINT_DEBUG == True):
+            print("\nDEBUG: binstring_sc: ")
+            print(numpy.array(binstring_sc)) #basically works (so close, but lsbs are a little off), probably a rounding thing [prints whole thing for debugging]
+            #print(binstring_sc) #works, prints abbreviated version
     
     #now we need to handle both horizontal and vertical bitplaning
     if(NUM_ROWS == 1):
@@ -296,22 +339,37 @@ def initialization():
         #first, the quantized data
         vector_embedding_qr = binstring_qr[0]
         binsplit_qr = Pool().map(binarytolist, vector_embedding_qr)
-        #print(binsplit)#works
+        if(PRINT_DEBUG == True):
+            print("\nDEBUG: binsplit_qr: ")
+            print(numpy.array(binsplit_qr))#works
         
         #now we need to transpose the matrix
         Matrix_qr = torch.tensor(binsplit_qr)
         matrixXPU_qr = Matrix_qr.to(ACCELERATION_DEVICE)
         bitplanes_qr = matrixXPU_qr.T
-        #print(bitplanes.to("cpu"))#works
+        if(PRINT_DEBUG == True):
+            print("\nDEBUG: bitplanes_qr: ")
+            print(bitplanes_qr)
+        #print(bitplanes_qr.to("cpu"))#works
         
         #now analyze the compressibility of the bitplanes
+        print("\n[single horizontal quant, quant] HORIZONTAL BITPLANE COMPRESSION (Single plane) (-->0 good): ")
         comp_qr = horizontal_bitplane_compressibility(bitplanes_qr)
+        print(comp_qr)
+        
+        print("\n[single horizontal quant, quant] Average Compression Ratio: ")
+        avg_sr = torch.mean(comp_qr)
+        print(avg_sr)
         
         #now, the scale data
         vector_embedding_sr = binstring_sr
         binsplit_sr = Pool().map(binarytolist, vector_embedding_sr)
-        #print(binsplit)#works
+        if(PRINT_DEBUG == True):
+            print("\nDEBUG: binsplit_sr: ")
+            print(binsplit_sr)#works
         
+        #since the scale only has 1 value, we cannot decompose it into bitplanes for compression analysis
+        '''
         #now we need to transpose the matrix
         Matrix_sr = torch.tensor(binsplit_sr)
         matrixXPU_sr = Matrix_sr.to(ACCELERATION_DEVICE)
@@ -320,6 +378,7 @@ def initialization():
         
         #now analyze the compressibility of the bitplanes
         comp_sr = horizontal_bitplane_compressibility(bitplanes_sr)
+        '''
         pass
     elif(NUM_ROWS > 1):
         #going to try a similar method as before on cpu, but it will be much slower due to multiple rows
@@ -328,57 +387,104 @@ def initialization():
         #binstring_qc
         #first, try it with quantized data (row-wise)
         binsplit_qr = Pool().map(binaryvectortolist, binstring_qr)
-        print("\nDEBUG: binsplit_qr: ")
-        print(binsplit_qr)
+        if(PRINT_DEBUG == True):
+            print("\nDEBUG: binsplit_qr: ")
+            print(binsplit_qr)
     
         #then transpose time
         matrix_qr = numpy.stack(binsplit_qr, axis=-1)
-        #print(matrix)
+        if(PRINT_DEBUG == True):
+            print("\nDEBUG: matrix_qr: ")
+            print(matrix_qr)
         
         comp_qr = vertical_bitplane_compressibility(matrix_qr, NUM_DIMS)
         print("\n[horizontal quant, quant] MULTI VECTOR VERTICAL BIT PLANE COMPRESSION RATIOS [Run Length Encoding](-->0 good): ")
         print(comp_qr)
         
+        print("\n[horizontal quant, quant] AVERAGE COMPRESSION RATIOS PER BIT PLANE: ")
+        avg_qr = torch.mean(comp_qr, dim=0)
+        print(avg_qr)
+        #print("\nexplanation of results above:\n - leftmost column is MSB\n - rightmost column is LSB\n - There is a single row, gets average of all 768 dimensions' bit planes of the # of rows you selected\n - compression ratio -> 0 means perfect compressibility, >1 means negative compressibility (takes more space to compress it than original)")
+
+        print("\n[horizontal quant, quant] AVG COMPRESSION RATIO PER VECTOR DIM: ")
+        avg2_qr = torch.mean(comp_qr, dim=1)
+        print(avg2_qr)
+        
+        print("\n[horizontal quant, quant] AVG COMPRESSION RATIO TOTAL BLOCK: ")
+        avg3_qr = torch.mean(avg_qr, dim=0)
+        print(avg3_qr)
+        
         #then try it with scales (row-wise)
         binsplit_sr = Pool().map(binaryvectortolist, binstring_sr)
-        print("\nDEBUG: binsplit_sr: ")
-        print(binsplit_sr)
+        if(PRINT_DEBUG == True):
+            print("\nDEBUG: binsplit_sr: ")
+            print(binsplit_sr)
     
         #then transpose time
         matrix_sr = numpy.stack(binsplit_sr, axis=-1)
-        #print(matrix)
+        if(PRINT_DEBUG == True):
+            print("\nDEBUG: matrix_sr: ")
+            print(matrix_sr)
         
         comp_sr = vertical_bitplane_compressibility(matrix_sr, NUM_DIMS)
         print("\n[horizontal quant, scale] MULTI VECTOR VERTICAL BIT PLANE COMPRESSION RATIOS [Run Length Encoding](-->0 good): ")
         print(comp_sr)
         
+        print("\n[horizontal quant, scale] AVERAGE COMPRESSION RATIO ACROSS ALL BIT PLANES ACROSS ALL ROWS/VECTORS: ")
+        avg_sr = torch.mean(comp_sr)
+        print(avg_sr)
+        
         #now we also need to account for vertical symmetric quantization too
         if(MULTI_SCALAR == True):
             #first, try it with quantized data (col-wise)
             binsplit_qc = Pool().map(binaryvectortolist, binstring_qc)
-            print("\nDEBUG: binsplit_qc: ")
-            print(binsplit_qc)
+            #print("\nDEBUG: binsplit_qc: ")
+            #print(binsplit_qc)
     
             #then transpose time
             matrix_qc = numpy.stack(binsplit_qc, axis=-1)
-            #print(matrix)
+            if(PRINT_DEBUG):
+                print("\nDEBUG: matrix_qc: ")
+                print(matrix_qc)
         
             comp_qc = vertical_bitplane_compressibility(matrix_qc, NUM_DIMS)
             print("\n[vertical quant, quant]MULTI VECTOR VERTICAL BIT PLANE COMPRESSION RATIOS [Run Length Encoding](-->0 good): ")
             print(comp_qc)
+            
+            print("\n[vertical quant, quant] AVERAGE COMPRESSION RATIOS PER BIT PLANE: ")
+            avg_qc = torch.mean(comp_qc, dim=0)
+            print(avg_qc)
+            #print("\nexplanation of results above:\n - leftmost column is MSB\n - rightmost column is LSB\n - There is a single row, gets average of all 768 dimensions' bit planes of the # of rows you selected\n - compression ratio -> 0 means perfect compressibility, >1 means negative compressibility (takes more space to compress it than original)")
+        
+            print("\n[vertical quant, quant] AVG COMPRESSION RATIO PER DIM OF ALL VECTOR EMBEDDINGS: ")
+            avg2_qc = torch.mean(comp_qc, dim=1)
+            print(avg2_qc)
+            
+            print("\n[vertical quant, quant] AVG COMPRESSION RATIO TOTAL BLOCK: ")
+            avg3_qc = torch.mean(avg_qc, dim=0)
+            print(avg3_qc)
         
             #then try it with scales (row-wise)
             binsplit_sc = Pool().map(binaryvectortolist, binstring_sc)
-            print("\nDEBUG: binsplit_sc: ")
-            print(binsplit_sc)
+            if(PRINT_DEBUG == True):
+                print("\nDEBUG: binsplit_sc: ")
+                print(binsplit_sc)
     
             #then transpose time
+            binsplit_sc = binsplit_sc[0]
             matrix_sc = numpy.stack(binsplit_sc, axis=-1)
-            #print(matrix)
+            if(PRINT_DEBUG == True):
+                print("\nDEBUG: matrix_sc: ")
+                print(matrix_sc)
         
-            comp_sc = vertical_bitplane_compressibility(matrix_sc, NUM_DIMS)
-            print("\n[vertical quant, scale] MULTI VECTOR VERTICAL BIT PLANE COMPRESSION RATIOS [Run Length Encoding](-->0 good): ")
-            print(comp_sc)
+            #comp_sc = vertical_bitplane_compressibility(matrix_sc, NUM_DIMS)#causes issues here, might try horizontal bitplane compressibility
+            comp_sc = horizontal_bitplane_compressibility(torch.from_numpy(matrix_sc)) #works
+            #through analysis, the structure of matrix_sc is the same as single vector horizontal bit plane compression, so I can verify that it works
+            print("\n[vertical quant, scale] MULTI VECTOR HORIZONTAL BIT PLANE COMPRESSION RATIOS [Run Length Encoding](-->0 good): ")
+            print(comp_sc) #confirmed to work correctly
+            
+            print("\n[vertical quant, scale] AVERAGE COMPRESSIBILITY BETWEEN ALL COMPRESSION RATIOS [RLE]: ")
+            print(torch.mean(comp_sc))
             pass
         pass
     pass
@@ -386,9 +492,10 @@ def initialization():
 #for thread safety
 if __name__ == '__main__':
     initialization()
-    #TO DO LIST FOR 2/7/26+:
-    # - KNOWN ISSUE: the binary plane splits used right before the compressibility have formatted it wrong, look at binsplit_qc vs the array under tensor308
+    #TO DO LIST FOR 2/8/26+:
+    # - I already checked the correctness of (multiscalar stuff for the most part, and some other main stuff), but make sure the rest is correct?
     # - once that is ironed out, we make versions of the compressibility analytics that use LZ4 and ZSTD per row,column, etc (with block sizes being customizable)
+    # - add print information under each results to explain how to interpret the results
     # - work on developing the other steps
     
     #once the main stuff is down: we can add the nice stuff:
